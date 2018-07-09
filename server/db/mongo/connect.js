99a9432bd7fb08c9f28c96cbacbbe36b4cf57475
xiaocong <xiaocong@gmail.com>
2018-07-09 15:30:47 +0800

    fix build error

--- server/db/mongo/connect.js
+++ server/db/mongo/connect.js
@@ -6,7 +6,7 @@ export default () => {
   // Find the appropriate database to connect to, default to localhost if not found.
   const connect = () => {
     mongoose.Promise = require('bluebird');
-    mongoose.connect(db, (err) => {
+    mongoose.connect(db, { useMongoClient: true }, (err) => {
       if (err) {
         console.log(`===>  Error connecting to ${db}`);
         console.log(`Reason: ${err}`);
